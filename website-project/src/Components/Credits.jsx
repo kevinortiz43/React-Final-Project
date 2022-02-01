a888037832e63c99892816a1581452b975ed5ad7
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-01-31 23:07:42 -0500

    Updated Code

--- website-project/src/Components/Credits.jsx
+++ website-project/src/Components/Credits.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function Credits() {
   return (
-    <div>
+    <div className="Credits-Container">
       
       <h2 className="Home">
         Credits to Free-To-Play Games Data Base by Digiwalls Media for the use of their API. 
-        Check them out at <a href={`https://www.freetogame.com/`}> Free-to-game</a>
+        Check them out at: <br/> <a href={`https://www.freetogame.com/`}> Free-to-game</a>
 
       
       </h2>
       
       <h2 className="Home">
         Big Credit to CheapShark API for the freedom and multiple options for all their video games. 
-        They are great and you should check them out at:
+        They are great and you should check them out at: <br/>
          <a href={`https://www.cheapshark.com`}>Cheap shark</a>
         </h2>
     </div>
