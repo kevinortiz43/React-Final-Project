669acb557e295d66d841de764a3839945c3434ae
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-02-01 11:41:49 -0500

    Updated Navigation bar

--- website-project/src/Components/Nav.jsx
+++ website-project/src/Components/Nav.jsx
@@ -4,9 +4,9 @@ export default function Nav() {
   return (
     <nav>
       <ul className="nav-links">
-        <NavLink style={{textDecoration:"none", color:"white"}} to="credits"><h1>Credits</h1></NavLink>
         <NavLink style={{textDecoration:"none", color: "white"}} to="free"><h1>List of free games</h1></NavLink>
         <NavLink style={{textDecoration:"none", color: "white"}} to="deals"><h1>List of Deals</h1></NavLink>
+        <NavLink style={{textDecoration:"none", color:"white"}} to="credits"><h1>Credits</h1></NavLink>
         <NavLink style={{textDecoration:"none", color: "white"}} to="contact"><h1>Contact us</h1></NavLink>
       </ul>
     </nav>
