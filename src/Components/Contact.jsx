fd7b2b3b9aa52ea09aee16f5fee8c75fcdc3bf89
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-02-02 23:51:57 -0500

    Updated Contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
   return (
     <div className="Contacts">
       <h1>Contact information:</h1>
-      <h2>phone number: 123-456-7890</h2>
-      <h2>Email: VideoGames@aol.com</h2>
+      <h2>phone number: 646-531-0537</h2>
+      <h2>Email: kevin.ortiz.software@gmail.com</h2>
     </div>
   );
 }
